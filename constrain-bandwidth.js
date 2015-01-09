6f6a21e6a34d6852214df938aa0e94bea3ff3bcf
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-09 11:43:45 +1100

    Initial implementation complete

--- constrain-bandwidth.js
+++ constrain-bandwidth.js
@@ -11,13 +11,21 @@ var tools = require('./tools');
 module.exports = function(value, mediaType) {
   return function(sdp) {
     var lines = tools.split(sdp);
+    var lineRegex = new RegExp(mediaType ? ('^m=' + mediaType) : '^s=');
+    var locations;
 
     // remove existing b=AS lines
     lines = lines.filter(function(line) {
       return line.slice(0, 3) !== 'b=AS'
     });
 
-    console.log(lines);
+    // find the splice locations for the bandwidth lines
+    locations = tools.locate(lineRegex, lines);
+
+    // iterate through the locations and add the new b=AS lines
+    locations.forEach(function(idx) {
+      lines.splice(idx + 1, 0, 'b=AS:' + value);
+    });
 
     return tools.join(lines);
   };
