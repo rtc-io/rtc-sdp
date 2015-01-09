7402ca3bf2e5bad60c6826886d67182ad2c227d5
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-09 11:43:29 +1100

    Added locate function to assist finding specific lines in the sdp

--- tools.js
+++ tools.js
@@ -1,9 +1,24 @@
 var reLineBreak = /\r?\n/;
 
+exports.join = function(lines) {
+  return lines.join('\n') + '\n';
+};
+
+// the locate function parses the lines and returns an array of line indexes of matches
+// the matches are returned in reverse order so that new lines can be spliced in without
+// invalidating the previously found locations
+exports.locate = function(regex, lines) {
+
+  function isMatch(line, idx) {
+    return regex.test(line) && idx;
+  }
+
+  return (lines || []).map(isMatch).filter(function(val) {
+    return typeof val == 'number';
+  }).reverse();
+};
+
 exports.split = function(sdp) {
   return sdp.split(reLineBreak).filter(Boolean);
 };
 
-exports.join = function(lines) {
-  return lines.join('\n') + '\n';
-};
