b68f413c431c61b6939cdbd084e3546b726419ed
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-10 16:07:06 +1100

    Additional tests for checking that we can extract the media types from sdp successfully

--- test/all.js
+++ test/all.js
@@ -1,3 +1,4 @@
 require('./parse');
 require('./candidates-add-video');
 require('./constrain-bandwidth');
+require('./count-lines');
---
+++ test/count-lines.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var test = require('tape');
+var baseSdp = fs.readFileSync(__dirname + '/fragments/test-answer-nodata.txt', 'utf8');
+var parse = require('..');
+var sdp;
+
+test('can parse the target sdp', function(t) {
+  t.plan(1);
+  t.ok(sdp = parse(baseSdp), 'created');
+});
+
+test('can determine that we have an audio and video line only in the sdp', function(t) {
+  t.plan(1);
+  t.deepEqual(sdp.getMediaTypes(), ['audio', 'video'], 'ok');
+});
