6b827fe32eff880412ed5ae09d12d3d76cd27485
neilyoung <neilyoung@users.noreply.github.com>
2021-11-21 17:05:15 +0100

    Prefer the first appearance of a particular codec...
    ...not the last one

--- prefer-codecs.js
+++ prefer-codecs.js
@@ -42,10 +42,12 @@ module.exports = function(desiredCodecs, mediaType) {
     // Build the codec definition
     var codec = result[2];
     var typeNum = result[1];
-    r[codec.toUpperCase()] = {
-      num: typeNum,
-      codec: codec
-    }
+    // Use the first appearance of the specific codec, not the last
+    if (!r[codec.toUpperCase()])
+      r[codec.toUpperCase()] = {
+        num: typeNum,
+        codec: codec
+      }
     return r;
   }
 
